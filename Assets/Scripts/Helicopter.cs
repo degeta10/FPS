4709654320c7d8f5484e3bf0644d443aa6617940
Devender Gupta <degeta10@gmail.com>
2017-12-11 23:29:25 +0530

    Second Commit

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+	public AudioClip audioclip;
+
+	private bool call = false;
+	private AudioSource audiosource;
+	// Use this for initialization
+	void Start () {
+		audiosource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown("CallHeli")&& call==false) {
+			call = true;
+			Debug.Log ("Helicopter Called");
+			audiosource.clip = audioclip;
+			audiosource.Play ();
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Transform SpawnPoints;
+	public bool Respawn=false;
+
+	private Transform[] spawnpoints;
+	private bool toggle = false;
+
+	void Start () {
+		spawnpoints = SpawnPoints.GetComponentsInChildren<Transform> ();
+	}
+	
+	private void ReSpawn()
+	{
+		int i = Random.Range (1,spawnpoints.Length);
+		transform.position = spawnpoints [i].transform.position;
+	}
+
+	void Update () 
+	{
+		if (toggle != Respawn) {
+			ReSpawn ();
+			Respawn = false;
+		} else {
+			toggle = Respawn;
+		}
+	}
+}
